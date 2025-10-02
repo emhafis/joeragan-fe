aee08bd443251c607b37ad8e04bcc78df398f77d
Muhammad Hafidz Arbanov <apitarbanov04@gmail.com>
2025-10-02 14:49:14 +0700

    styling projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,8 +5,8 @@ export default function Projects() {
         <h2 className="text-6xl font-extrabold text-white">
           Our Projects
         </h2>
-        <p className="mt-4 md:mt-0 text-base sm:text-lg text-gray-700 md:pl-4 lg:pl-12">
-          <span className="font-bold text-primary">Joeragan Properti</span>{" "}
+        <p className="mt-4 md:mt-0 text-base sm:text-lg text-white md:pl-4 lg:pl-12">
+          <span className="font-bold text-white">Joeragan Properti</span>{" "}
           merupakan perusahaan yang berfokus memberikan solusi bagi kebutuhan
           jasa seputar properti di Aceh. Informasi dan produk layanan yang
           diberikan cukup beragam, seperti:
