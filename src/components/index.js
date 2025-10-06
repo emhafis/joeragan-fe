643a9a0b47a3d7b31dc964e100d26ab63846bc2b
Muhammad Hafidz Arbanov <apitarbanov04@gmail.com>
2025-10-06 14:09:36 +0700

    change

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import HeroSection from "./HeroSection";
-import ServiceSection from "./ServiceSection";
-import ProjectSection from "./ProjectSection"
-import LocationSection from "./LocationSection";
-import TestimonialSection from "./TestimonialSection";
+import HeroSection from "./home/HeroSection";
+import ServiceSection from "./home/ServiceSection";
+import ProjectSection from "./home/ProjectSection"
+import LocationSection from "./home/LocationSection";
+import TestimonialSection from "./home/TestimonialSection";
 
 export {
   Header,
